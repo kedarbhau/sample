63d401cfb62e5ed1109fab9b1bf923029f742809
kedarbhau <kedarbhau@kedarbhau-23-f202in>
2019-07-21 11:32:39 +0530

    First github commit

---
+++ javaproject/src/javaproject/test/HelloWorld.java
@@ -0,0 +1,23 @@
+package javaproject.test;
+
+public class HelloWorld {
+	static String name = "Test";
+	
+	public HelloWorld() 
+	{
+		name = new String("Constructor");
+		//name = new String("Constructor again");
+	}
+	public HelloWorld(String nameChange)
+	{
+		name = nameChange;
+	}
+
+	public static void main(String[] args) 
+	{
+		HelloWorld hello = new HelloWorld();
+		System.out.println("Hello World - This is first Java program\n" + name);
+
+	}
+
+}
---
+++ javaproject/src/javaproject/test/Retirement.java
@@ -0,0 +1,35 @@
+package javaproject.test;
+
+import java.util.Scanner;
+
+public class Retirement {
+
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("How much money you need to retire");
+		double goal = in.nextDouble();
+		
+		System.out.println("How much money you would contribute every year");
+		double payment = in.nextDouble();
+		
+		System.out.println("Interest Rate in % : ");
+		double interestRate = in.nextDouble();
+		
+		double balance = 0;
+		int years = 0;
+		
+		while(balance < goal)
+		{
+			balance += payment;
+			double interest = balance * interestRate / 100;
+			balance += interest;
+			years++;
+		}
+		
+		System.out.println("You can retire in  " + years + " Years");
+					
+	}
+
+}
